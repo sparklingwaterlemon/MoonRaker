62d13530c4301dfa88b898731d1d04f88a2a8704
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-16 18:56:22 -0800

    Created BrowserRouter path.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 
 import App from './App';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
