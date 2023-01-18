57e691c36782ca48d5853077d5b81eb48f5850a2
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-17 17:37:30 -0800

    About Page complete for now

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -4,9 +4,31 @@ export default function AboutPage(){
     return(
         <section id="about-section">
             <div className="about-container">
-                <span className="about-info">
-                    ABOUT
-                </span>
+                <div className="about-links">
+                    <span className="about-header">
+                        Michael Kim 2023
+                    </span>
+                </div>
+                <div className="about-links">
+                    <a href="https://www.youtube.com/watch?v=C8Xc0VptCBY" target="_blank" rel="noreferrer" className="about-href">
+                        The Great Gig In The Sky - Pink Floyd
+                    </a>
+                </div>
+                <div className="about-links">
+                    <a href="https://github.com/sparklingwaterlemon/MoonRaker" target="_blank" rel="noreferrer" className="about-href">
+                    GitHub
+                    </a>
+                </div>
+                <div className="about-links">
+                    <a href="https://www.linkedin.com/in/michaelkim3/" target="_blank" rel="noreferrer" className="about-href">
+                        LinkedIn
+                    </a>
+                </div>
+                <div className="about-links">
+                    <a href="https://michaelkim.netlify.app/" target="_blank" rel="noreferrer" className="about-href">
+                        Portfolio
+                    </a>
+                </div>
             </div>
         </section>
     )
