679bdc225acefbf2a1c600c6255cb290cf0f7d07
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-18 18:00:24 -0800

    created journal entry button

--- src/sections/FormSection/FormSection.jsx
+++ src/sections/FormSection/FormSection.jsx
@@ -6,8 +6,10 @@ import { Link } from "react-router-dom";
 export default function FormSection({setCityLocation, setWeatherAstroData, setFlipAB}){
     return(
         <section id="form-section">
-            <div className="log-container">
-
+            <div className="journal-link-container">
+                <Link to="about" className="journal-link">
+                    Journal Entry
+                </Link>
             </div>
             <SearchBarComponent 
                 setCityLocation={setCityLocation} 
