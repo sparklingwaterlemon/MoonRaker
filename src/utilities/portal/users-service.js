3a18243194963fdd40c9623dc7def25cb12a70ca
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-25 19:53:12 -0800

    Saved token to localstorage. Retrieved token from localstorange and JSON.parsed. In process of setting up the User useState. Deciding where to locate and how to toggle pages.

--- src/utilities/portal/users-service.js
+++ src/utilities/portal/users-service.js
@@ -5,8 +5,30 @@ export async function signUp(userData){
         console.log("curr @ user-service.js signUp -> ");
         const token = await usersAPI.signUp(userData)
         console.log("<- curr @ user-service.js \\ user-api.js")
-        return token;
+        localStorage.setItem('token', token);
+        return getUser();
     } catch{
         throw new Error("user-service.js - Invalid Sign Up")
     };
+};
+
+
+export function getToken(){
+    const token = localStorage.getItem('token');
+
+    // getItem returns null if there's no string
+    // checking if no token or if expired
+    if(!token) return null;
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    if (payload.exp < Date.now() / 1000){
+        localStorage.removeItem('token');
+        return null;
+    };
+    return token;
+};
+
+// separating retreiving token vs passing along user
+export function getUser(){
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split(".")[1])).user : null;
 };
