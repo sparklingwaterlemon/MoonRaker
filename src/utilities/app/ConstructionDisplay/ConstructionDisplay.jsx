85dd35c48e3fc7767083905765cb9e53b1beb1af
sparklingwaterlemon <michaelkim009@gmail.com>
2023-01-26 21:27:28 -0800

    construction sign display

--- src/utilities/app/ConstructionDisplay/ConstructionDisplay.jsx
+++ src/utilities/app/ConstructionDisplay/ConstructionDisplay.jsx
@@ -37,6 +37,7 @@ export default function ConstructionDisplay() {
                     Update - Created Sign In && Log Out functionality!
                     <br/>
                     <h4>Website is best experienced through the Chrome Browser</h4>
+                    coming soon - journal entry capabilities
 
                     <div className="c-links">
                         <a href="https://github.com/sparklingwaterlemon/The-Great-Gig-in-the-Sky" target="_blank" rel="noreferrer">github</a>
