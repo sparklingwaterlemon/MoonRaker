1b7191871b767b74d5533b7eadc6bfa26273ff5f
sparklingwaterlemon <michaelkim009@gmail.com>
2023-02-07 21:18:57 -0800

    stubbed up a checktoken function following tutorial.. unnecessary but a bit for practice.

--- src/pages/JournalPage/JournalPage.jsx
+++ src/pages/JournalPage/JournalPage.jsx
@@ -1,12 +1,21 @@
 import "./JournalPage.css";
 import PortalNavBar from "../../components/portal/PortalNavBar/PortalNavBar";
+import * as UserService from "../../utilities/portal/users-service";
 
 export default function JournalPage({ setUser }){
+    async function handleCheckToken(){
+        const expUnix = await UserService.checkToken();
+        const expDate = new Date(expUnix * 1000);
+        alert(expDate.toString());
+    };
+
+
     return(
         <section id="journal-page">
             <PortalNavBar setUser={setUser}/>
             
             <h1> Journal Page</h1>
+            <button onClick={handleCheckToken}> Check when login expires </button>
         </section>
     )
 }
--- src/utilities/portal/users-service.js
+++ src/utilities/portal/users-service.js
@@ -1,51 +1,54 @@
 import * as usersAPI from "./users-api";
 
-export async function signUp(userData){
-    try{
+export async function signUp(userData) {
+    try {
         const token = await usersAPI.signUp(userData)
         localStorage.setItem('token', token);
         return getUser();
-    } catch(err){
+    } catch (err) {
         throw new Error(err.message);
     };
 };
 
 
-
-export function getToken(){
+export function getToken() {
     const token = localStorage.getItem('token');
 
     // getItem returns null if there's no string
     // checking if no token or if expired
-    if(!token) return null;
+    if (!token) return null;
     const payload = JSON.parse(atob(token.split(".")[1]));
-    if (payload.exp < Date.now() / 1000){
+    if (payload.exp < Date.now() / 1000) {
         localStorage.removeItem('token');
         return null;
     };
     return token;
 };
-
 // separating retreiving token vs passing along user
-export function getUser(){
+export function getUser() {
     const token = getToken();
     // if token is valid
     return token ? JSON.parse(atob(token.split(".")[1])).user : null;
 };
 
 
-export async function login(cred){
-    try{
+export async function login(cred) {
+    try {
         const token = await usersAPI.login(cred);
         localStorage.setItem('token', token)
         return getUser();
-    } catch(err){
+    } catch (err) {
         throw new Error(err.message);
     };
 };
 
 
-
-export async function logOut(){
+export async function logOut() {
     localStorage.removeItem('token');
 };
+
+
+export async function checkToken(){
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split(".")[1])).exp : null;
+}
